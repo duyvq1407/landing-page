5c72a8b3577301985d9edea74410300fbdc0a1e1
duyvq1407 <duyvq1407@gmail.com>
2022-10-18 11:52:08 +0700

    slick slider

--- assets/js/index.js
+++ assets/js/index.js
@@ -60,12 +60,38 @@ function openNavbar() {
     }
 }
 
-// window.onresize = () => {
-//     if (window.innerWidth >= 870) {
-//         openBtn.style.display = 'none';
-//         layoutBlur.style.display = 'none';
-//         closeBtn.style.display = 'none';
-//     } else {
-//         openBtn.style.display = 'none';
-//     }
-// };
+$('.slick-reponsive').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    nextArrow: '.case_study-next-btn',
+    prevArrow: '.case_study-prev-btn',
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true,
+            },
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            },
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            },
+        },
+    ],
+});
